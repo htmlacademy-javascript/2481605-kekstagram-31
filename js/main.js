522cb78bce20931a2abdd3ad01e692d1457a7d2b
Sasha <pratov99@gmail.com>
2024-02-27 13:10:55 +0300

    Решил проблему

--- js/main.js
+++ js/main.js
@@ -52,5 +52,4 @@ const createUserPhoto = () => ({
     Array.from({length: getRandomInteger(0, 30)}, createComment)
 });
 const similarPhotos = Array.from({length: SIMILAR_PHOTOS_COUNT}, createUserPhoto);
-similarPhotos();
-console.log(similarPhotos);
+similarPhotos.values();
