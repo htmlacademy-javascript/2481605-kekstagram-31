67d2d98c90aae7fcf7f07f2165018937f8d0857a
Sasha <pratov99@gmail.com>
2024-04-01 02:01:30 +0300

    Перламутровые пуговицы

--- js/util.js
+++ js/util.js
@@ -1,17 +1,8 @@
-import {bodyElement, dataErrorTemplate} from './search-elements';
+import {bodyElement, dataErrorTemplate, imgFiltersInactiveElement} from './search-elements';
 
 const TIME_OUT = 5000;
 
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 const isEscapeKey = (evt) => evt.key === 'Escape';
-const isEnterKey = (evt) => evt.key === 'Enter';
 
 const modalOpenAdd = () => {
   bodyElement.classList.add('modal-open');
@@ -31,14 +22,25 @@ const showErrorMessage = () => {
   }, TIME_OUT);
 };
 
+const opacityOne = () => {
+  imgFiltersInactiveElement.style.opacity = '1';
+};
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
 export {
-  getRandomInteger,
-  getRandomArrayElement,
   isEscapeKey,
-  isEnterKey,
   closeElement,
   showElement,
   modalOpenAdd,
   modalOpenRemove,
   showErrorMessage,
+  opacityOne,
+  debounce
 };
